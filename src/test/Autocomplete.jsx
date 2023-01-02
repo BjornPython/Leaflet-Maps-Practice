8d9457f423ceb96c9057f76534eb9799361fa974
Nathan <nathanflores887@gmail.com>
2023-01-02 14:01:12 +0800

    added searching function for maps

--- src/test/Autocomplete.jsx
+++ src/test/Autocomplete.jsx
@@ -3,24 +3,57 @@ import Location from "./Location"
 //14.6113338545211, 121.11348532629556
 
 function Autocomplete() {
-    const NOMINATIM_ENDPT = "https://nominatim.openstreetmap.org/"
+    const NOMINATIM_ENDPT = "https://nominatim.openstreetmap.org"
 
 
-    const [locRes, setLocRes] = useState([{ "place_id": 1234, "display_name": "Cainta Rizal" }, { "place_id": 7531, "display_name": "MARIKINA RIZAL" }])
+    const [locRes, setLocRes] = useState([])
+    const [inputLocation, setInputLocation] = useState({
+        searchedLoc: ""
+    })
 
+    const { searchedLoc } = inputLocation
 
     console.log(locRes);
+
+    const addLoc = (e) => {
+        e.preventDefault()
+        // setLocRes([{ "place_id": 1234, "display_name": "Cainta Rizal" }, { "place_id": 7531, "display_name": "MARIKINA RIZAL" }])
+        setLocRes(searchLocations)
+    }
+
+    const changeFormData = (e) => {
+        setInputLocation((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const searchLocations = async (e) => {
+        e.preventDefault()
+        console.log("SEARCING");
+        const params = new URLSearchParams({
+            q: searchedLoc,
+            format: "json",
+            addressdetails: 1
+        })
+        const locations = await fetch(`${NOMINATIM_ENDPT}/search?${params}`).then(response => response.json())
+        console.log("RESPONSE: ", locations);
+        setLocRes(locations)
+
+        return locations
+    }
+
+
 
 
     return (
         <div>
-            <form action="">
-                <input type="text" />
+            <form action="" onSubmit={searchLocations}>
+                <input type="text" name="searchedLoc" value={searchedLoc} onChange={changeFormData} />
                 <button type='submit'>SUBMIT</button>
             </form>
 
             {locRes.map((loc) => {
-                console.log("LOC: ", loc);
                 return <Location key={loc.place_id} location={loc.display_name} />
             })}
 
